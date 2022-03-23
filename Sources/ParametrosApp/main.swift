18dbc2d8060def80a29755d2b1c99bd37908eff5
NarelyLima <narely148@gmail.com>
2022-03-23 17:38:08 -0300

    Coloquei funcoes de verificacao e melhorei os filtros

--- Sources/ParametrosApp/main.swift
+++ Sources/ParametrosApp/main.swift
@@ -12,7 +12,7 @@ struct Local: Decodable {
     let clima: String
     let regiao: String
     let tipoAmbiente: String
-    let comida: String
+    var comida: String
     let urlLocal: String
     let urlHotel: String
     let urlFood: String
@@ -37,7 +37,7 @@ struct Local: Decodable {
 }
 
 
-func verificar(){
+func verificarResp(){
     while true{
         if  yonFood != "n"  && yonFood != "s"  && yonFood != "nao"  && yonFood != "sim"{
             print("escreve direito ai mash")
@@ -48,6 +48,73 @@ func verificar(){
     }
 }
 
+func verificarRegion() {
+    var locaisPorRegiao = filterRegiao(region, locais: locaisPorClima)
+    
+    if locaisPorRegiao.isEmpty {
+        while locaisPorRegiao.isEmpty == true  {
+            print("nao existe. Digite outra opcao:")
+            region = readLine()!.lowercased()
+            if (region == "a") {
+                region = "norte"
+            } else if (region == "b") {
+                region = "nordeste"
+            }else if (region == "c") {
+                region = "sul"
+            }else if (region == "d") {
+                region = "sudeste"
+            }else if (region == "e") {
+                region = "centro oeste"
+            }
+            locaisPorRegiao = filterRegiao(region, locais: locaisPorClima)
+        }
+    }
+    for local in locaisPorRegiao{
+        print(local.printParametros())
+    }
+    
+}
+
+func verificarAmbiente() {
+    var locaisPorAmbiente = filterAmb(typeAmb, locais: locaisPorRegiao)
+    
+    if locaisPorAmbiente.isEmpty {
+        while locaisPorAmbiente.isEmpty == true  {
+            print("nao existe. Digite outra opcao:")
+            typeAmb = readLine()!.lowercased()
+            if (typeAmb == "a") {
+                typeAmb = "praia"
+            } else if (typeAmb == "b") {
+                typeAmb = "serra"
+            }else if (typeAmb == "c") {
+                typeAmb = "interior"
+            }else if (typeAmb == "d") {
+                typeAmb = "floresta"
+            }else if (typeAmb == "e") {
+                typeAmb = "cidade grande"
+            }
+            locaisPorAmbiente = filterAmb(typeAmb, locais: locaisPorRegiao)
+        }
+    }
+    for local in locaisPorAmbiente{
+        print(local.printParametros())
+    }
+    
+}
+
+func verificarComida() {
+    var locaisPorComida = filterComida(food, locais: locaisPorAmbiente)
+    if locaisPorComida.isEmpty {
+        while locaisPorComida.isEmpty == true {
+            print("nao existe. Digite outra opcao:")
+            
+            
+        }
+    }
+    for local in locaisPorComida{
+        print(local.printParametros())
+    }
+}
 // Parametros que estou usando para utilizar no meus switch/case
 
 let arquivoURL = Bundle.module.url(forResource: "locais", withExtension: "json")!
@@ -56,19 +123,19 @@ var listaDeLocais: [Local] = []
 do {
     let locaisData = try Data(contentsOf: arquivoURL)
     listaDeLocais = try JSONDecoder().decode([Local].self, from: locaisData)
-    print(listaDeLocais[0])
 } catch {
     print(error)
 }
 
-//print(listaDeLocais[0])
 
 // perguntas que serão realizadas ao meu usuário
 
+// ------------------ CLIMA -------------------------
+
 print("""
-Qual o clima que você quer? Quente(A) ou Frio(B)?
-Por favor, digite o nome ou a letra referente:
-""")
+    Qual o clima que você quer? Quente(A) ou Frio(B)?
+    Por favor, digite o nome ou a letra referente:
+    """)
 var climate = readLine()!.lowercased()
 let validOptions1 = ["a","b","quente","frio"]
 if !validOptions1.contains(climate) {
@@ -77,58 +144,93 @@ if !validOptions1.contains(climate) {
         climate = readLine()!.lowercased()
     }
     print("clima escolhido: \(climate)")
-} else {
-    print("clima escolhido: \(climate)")
+    
+    
 }
 
-print(filterClima(climate))
-//-------------------------------------------
+if (climate == "a") {
+    climate = "quente"
+} else if (climate == "b") {
+    climate = "frio"
+}
+
+var locaisPorClima = filterClima(climate, locais: listaDeLocais)
+for local in locaisPorClima{
+    print(local.printParametros())
+}
+//------------------ REGIAO -------------------------
 
 print("""
-Qual a região do Brasil que você quer conhecer?
-Por favor, digite o nome ou a letra referente:
-Norte(a), Nordeste(b), Sul(c), Sudeste(d), Centro oeste(e)
-""")
+    Qual a região do Brasil que você quer conhecer?
+    Por favor, digite o nome ou a letra referente:
+    Norte(a), Nordeste(b), Sul(c), Sudeste(d), Centro oeste(e)
+    """)
 var region = readLine()!.lowercased()
 let validOptions2 = ["a","b","c","d","e","norte","nordeste","sul","sudeste","centro oeste"]
+if (region == "a") {
+    region = "norte"
+} else if (region == "b") {
+    region = "nordeste"
+}else if (region == "c") {
+    region = "sul"
+}else if (region == "d") {
+    region = "sudeste"
+}else if (region == "e") {
+    region = "centro oeste"
+}
 if !validOptions2.contains(region){
     while !validOptions2.contains(region) {
         print("Insira novamente: Norte(a), Nordeste(b), Sul(c), Sudeste(d), Centro oeste(e)")
         region = readLine()!.lowercased()
     }
     print("A regiao foi: \(region)")
-} else {
-    print("A regiao foi: \(region)")
 }
 
-//----------------------------------------
+verificarRegion()
+
+var locaisPorRegiao = filterRegiao(region, locais: locaisPorClima)
+
+
+//------------------ TIPO DE AMBIENTE ----------------------
 
 print("""
-Qual o tipo de ambiente que voce deseja?
-Por favor, digite o nome ou a letra referente:
-Praia(a), Serra(b), Interior(c), Floresta(d), Cidade Grande(e)
-""")
+    Qual o tipo de ambiente que voce deseja?
+    Por favor, digite o nome ou a letra referente:
+    Praia(a), Serra(b), Interior(c), Floresta(d), Cidade Grande(e)
+    """)
 var typeAmb = readLine()!.lowercased()
-let validOptions3 = ["a","b","c","d","e","praia","serra","interior","floresta","Cidade Grande"]
+let validOptions3 = ["a","b","c","d","e","praia","serra","interior","floresta","cidade grande"]
+if (typeAmb == "a") {
+    typeAmb = "praia"
+} else if (typeAmb == "b") {
+    typeAmb = "serra"
+}else if (typeAmb == "c") {
+    typeAmb = "interior"
+}else if (typeAmb == "d") {
+    typeAmb = "floresta"
+}else if (typeAmb == "e") {
+    typeAmb = "cidade grande"
+}
 if !validOptions3.contains(typeAmb) {
     while !validOptions3.contains(typeAmb)  {
         print("Insira novamente: Praia(a), Serra(b), Interior(c), Floresta(d), Cidade Grande(e)")
         typeAmb = readLine()!.lowercased()
     }
     print("O tipo de ambiente escolhido foi: \(typeAmb)")
-} else{
-    print("O tipo de ambiente escolhido foi: \(typeAmb)")
 }
 
-//--------------------------------------------
+var locaisPorAmbiente = filterAmb(typeAmb, locais: locaisPorRegiao)
+
+//--------------------- TIPO DE COMIDA -----------------------
 var food: [String] = []
 print("""
     Você tem alguma vontade de comer algo específico? SIM(S) ou NAO(N)?
+    OBS.: Se você escolher, consideraremos todos os tipos de comida.
     """)
 var yonFood = readLine()!.lowercased()
 let validOptions4 = ["a", "b", "c", "d", "e", "f", "g", "japonesa", "italiana", "brasileira", "pizza", "mexicana", "sorvete", "sanduiche"]
 
-verificar()
+verificarResp()
 if yonFood == "s" || yonFood == "sim" {
     while yonFood == "s" || yonFood == "sim" {
         print("""
@@ -137,18 +239,31 @@ if yonFood == "s" || yonFood == "sim" {
             """)
         var typeFood = readLine()!.lowercased()
         
-        
         if !validOptions4.contains(typeFood) {
             while !validOptions4.contains(typeFood)  {
                 print("Insira novamente: Japonesa(A), Italiana(B), Brasileira(C), Pizza(D), Mexicana(E), Sorvete(F), Sanduiche(G)")
                 typeFood = readLine()!.lowercased()
             }
             print("A comida escolhida foi: \(typeFood)")
-            food.append(typeFood)
             
-        } else {
-            food.append(typeFood)
         }
+        if (typeFood == "a") {
+            typeFood = "japonesa"
+        } else if (typeFood == "b") {
+            typeFood = "italiana"
+        }else if (typeFood == "c") {
+            typeFood = "brasileira"
+        }else if (typeFood == "d") {
+            typeFood = "pizza"
+        }else if (typeFood == "e") {
+            typeFood = "mexicana"
+        }else if (typeFood == "f") {
+            typeFood = "sorvete"
+        }else if (typeFood == "g") {
+            typeFood = "sanduiche"
+        }
+        food.append(typeFood)
+        
         print("O(s) tipo(s) de comida(s) escolhido(s) foi(foram): \([food])")
         
         print("""
@@ -160,13 +275,15 @@ if yonFood == "s" || yonFood == "sim" {
     
 } else if yonFood == "n" || yonFood == "nao"{
     print("Consideraremos todos os tipos de comida")
+    food += ["japonesa","mexicana","brasileira","italiana","pizza","sanduiche","sorvete"]
 }
 
-print("O numero digitado foi: \([food])")
+print("A(s) comida(s) digitada(s) foi(ram): \([food])")
 
-
-
-
+let locaisPorComida = filterComida(food, locais: locaisPorAmbiente)
+for local in locaisPorComida{
+    print(local.printParametros())
+}
 
 //-----------------------------------------
 // Faremos os filtros por partes:
@@ -175,37 +292,30 @@ print("O numero digitado foi: \([food])")
 // filtra pelo tipo de ambiente
 // filtra pelo tipo de comida
 
-var climaList: [String]
-func filterClima(_ string: String) {
-    let climaList = listaDeLocais.filter{$0.clima == string}
-   climaList.forEach { travel in
-       print(travel.printParametros())
-   }
-}
 
-let regiaoList: [String]
-func filterRegiao(_ string: String) {
-    regiaoList = climaList.filter{$0.regiao == string}
-   regiaoList.forEach { travel in
-       print(travel.printParametros())
-   }
-}
 
-var ambList: [String]
-func filterAmb(_ string: String) {
-    ambList = regiaoList.filter{$0.tipoAmbiente == string}
-   ambList.forEach { travel in
-       print(travel.printParametros())
-   }
-}
-
-var comidaList: [String]
-func filterComida(_ string: String) {
-    comidaList = ambList.filter{$0.comida == string}
-   comidaList.forEach { travel in
-       print(travel.printParametros())
-   }
+func filterClima(_ string: String, locais: [Local]) -> [Local] {
+    let climaList = locais.filter{$0.clima == string}
+    return climaList
 }
 
 
-//
+func filterRegiao(_ string: String, locais: [Local]) -> [Local] {
+    let regiaoList = locais.filter{$0.regiao == string}
+    return regiaoList
+}
+
+
+func filterAmb(_ string: String, locais: [Local]) -> [Local] {
+    let ambList = locais.filter{$0.tipoAmbiente == string}
+    return ambList
+}
+
+
+func filterComida(_ comidas: [String], locais: [Local]) -> [Local] {
+    let comidaList = locais.filter{ comidas.contains($0.comida) }
+    return comidaList
+}
+
+
+
